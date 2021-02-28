7c342afeccd2fbf949a64fba7b582c8bcc23c88f
NacRyTchUk <god2god3@mail.ru>
2021-02-28 21:08:34 +0700

    stash

--- DottApp.Client/ViewModels/DebugViewModel.cs
+++ DottApp.Client/ViewModels/DebugViewModel.cs
@@ -66,12 +66,12 @@ namespace DottApp.Client.ViewModels
         #region Commands
 
         #region Api.Connect
+        RSAw rsaw = new RSAw();
 
         public ICommand GenerateRequestCommand { get; }
 
         private void OnGenerateRequestCommandExecuted(object param)
         {
-            RSAw rsaw = new RSAw();
             ConnectionSessionRequest csr = new ConnectionSessionRequest()
             {
                 IsFirstTime = true,
@@ -92,21 +92,51 @@ namespace DottApp.Client.ViewModels
             string baseUrl = ConfigurationManager.AppSettings["BaseApiUrl"];
             #if DEBUG
                 baseUrl = ConfigurationManager.AppSettings["BaseDebugApiUrl"];
-            #endif
+#endif
 
 
-            //RestClient client = new RestClient("http://api.dottapp.nrtu.studio");
-            // client.RemoteCertificateValidationCallback = (sender, ser, chain, sslPolicyErrors) => true;
-            //var req = new RestRequest("WeatherForecast/Connect");
-            //req.AddJsonBody(JsonSerializer.Deserialize<ConnectionSessionRequest>(ApiConnectRequestText));
-            //var resp = client.Post(req);
-            //ApiConnectResponseText =   resp.Content;
+            RestClient client = new RestClient(baseUrl);
+            client.RemoteCertificateValidationCallback = (sender, ser, chain, sslPolicyErrors) => true;
+            var req = new RestRequest("api/Auth/NewConnectSession");
+            req.AddHeader("Content-type", "application/json");
+            req.AddJsonBody(JsonSerializer.Deserialize<ConnectionSessionRequest>(ApiConnectRequestText));
+            var resp = client.Post(req);
+            ApiConnectResponseText = resp.Content;
 
-            RESTw restw = new RESTw(baseUrl);
-            var response = restw.Post<ConnectionSessionRequest>
-            ("WeatherForecast/Connect",
-                JsonSerializer.Deserialize<ConnectionSessionRequest>(ApiConnectRequestText));
-            ApiConnectResponseText = response.Content;
+            //RESTw restw = new RESTw(baseUrl);
+            //var response = restw.Post<ConnectionSessionRequest>
+            //("/api/Auth/NewConnectSession",
+            //    JsonSerializer.Deserialize<ConnectionSessionRequest>(ApiConnectRequestText));
+            //ApiConnectResponseText = response.Content;
+
+
+
+            var respVal = JsonSerializer.Deserialize<ConnectionSessionResponse>(resp.Content);
+            var reqVal = JsonSerializer.Deserialize<ConnectionSessionRequest>(ApiConnectRequestText);
+
+            MessageBox.Show(rsaw.Decrypt(respVal.SessionId));
+
+
+
+
+            //var jSonData = JsonConvert.SerializeObject(customObj);
+
+            //using (var httpClient = new HttpClient())
+            //{
+            //    httpClient.BaseAddress = new Uri("https://www.testapi.com");
+
+            //    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //    using (var response =
+            //        await httpClient.PostAsync(url, new StringContent(jSonData, Encoding.UTF8, "application/json")))
+            //    {
+            //        using (var content = response.Content)
+            //        {
+            //            var result = await content.ReadAsStringAsync();
+            //        }
+            //    }
+            //}
         }
 
         private bool CanSendRequestCommandExecute(object param) => true;
---
+++ DottApp.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DottApp.RsaAesWrapper;
+using DottApp.Services.Auth;
+
+namespace DottApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+
+        [HttpPost("NewConnectSession")]
+        public ConnectionSessionResponse Post([FromBody]ConnectionSessionRequest csRequest)
+        {
+            RSAw host_rsaw = new RSAw();
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            
+            string cl_accessToken = string.Empty;
+            for (int i = 0; i < 32; i++) cl_accessToken += (char)rnd.Next('a', 'z');
+
+            var csResponse = new ConnectionSessionResponse
+            {
+                PublicKey = new RSAByteKey().SetKeyFromParameters(host_rsaw.PublicKey),
+                AccessToken = host_rsaw.Encrypt("Токен"), // new AccessToken().GenNew()
+                SessionId = host_rsaw.Encrypt(rnd.Next(0, 100).ToString(), csRequest.PublicKey.GetRSAParameters())
+            };
+            return csResponse;
+        }
+
+
+
+
+    }
+}
