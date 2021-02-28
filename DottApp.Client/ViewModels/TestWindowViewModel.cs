05f2fef1f6cd3fddc78768cdeeda212b86ca1f0b
busyhedg03 <alexgrihalex@gmail.com>
2021-02-28 12:45:52 +0700

    Добавил на форму TestWindow текстовое поле и кнопку, по нажатию на которую будет выводится текст из поля в MessageBox и другой TextBox

---
+++ DottApp.Client/ViewModels/TestWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DottApp.Client.ViewModels.Base;
+using DottApp.Client.Views.Windows;
+using DottApp.Client.Infrastructure.Commands;
+using System.Windows.Input;
+using System.Windows;
+
+namespace DottApp.Client.ViewModels
+{
+    #region Тект
+    internal class TestWindowViewModel : ViewModel
+    {
+        private string _Text = "Введи сюда что-нибудь";
+        public string Text
+        {
+            get => _Text;
+            set => Set(ref _Text, value);
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand ShowMessageCommand { get; }
+
+        private void OnShowMessageCommandExecuted(object param = null)
+        {
+            MessageBox.Show(Text);
+        }
+
+        private bool CanShowMessageCommandExecute(object param = null) => true;
+
+
+        #endregion
+
+        public TestWindowViewModel()
+        {
+            #region Commands initialization
+            ShowMessageCommand = new LambdaCommand(OnShowMessageCommandExecuted, CanShowMessageCommandExecute);
+
+
+            #endregion
+        }
+    }
+}
