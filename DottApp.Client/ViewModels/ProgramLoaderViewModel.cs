0ff3306433dd30617c715943ac326d557e64db58
NacRyTchUk <god2god3@mail.ru>
2021-03-05 01:21:08 +0700

    +AESw

--- DottApp.Client/ViewModels/ProgramLoaderViewModel.cs
+++ DottApp.Client/ViewModels/ProgramLoaderViewModel.cs
@@ -8,6 +8,7 @@ using DottApp.Client.Infrastructure.Commands;
 using DottApp.Client.Properties;
 using DottApp.Client.ViewModels.Base;
 using DottApp.Client.Views.Windows;
+using DottApp.RsaAesWrapper;
 using DottApp.Services.Auth;
 
 
@@ -48,14 +49,14 @@ namespace DottApp.Client.ViewModels
         {
             if (param is null) return;
 
-            
 
-            //switch (Convert.ToInt32(param))
-            //{
-            //    case 1 : { new MainWindow().Show(); break; }
-            //    case 2 : { new DebugWindow().Show(); break; }
-            //    case 3 : { new TestWindow().Show(); break; }
-            //}
+
+            switch (Convert.ToInt32(param))
+            {
+                case 1: { new MainWindow().Show(); break; }
+                case 2: { new DebugWindow().Show(); break; }
+                case 3: { new TestWindow().Show(); break; }
+            }
         }
 
         private bool CanOpenWindowCommandExecute(object param) => true;
